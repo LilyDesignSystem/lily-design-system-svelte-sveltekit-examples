4638f27a07d6ce4c05ae0061ccad5d14a01bc7de
Joel Parker Henderson <joel@joelparkerhenderson.com>
2026-05-26 04:29:14 +0100

    Tune component lists

---
+++ etc/skel/components/example/Example.stories.svelte
@@ -0,0 +1,34 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+ 
+  import MyComponent from './Example.svelte';
+ 
+  const { Story } = defineMeta({
+    component: MyComponent,
+  });
+</script>
+ 
+<Story
+  name="MyStory"
+  parameters={{
+    sveltekit_experimental: {
+      state: {
+        page: {
+          data: {
+            test: 'passed',
+          },
+        },
+        navigating: {
+          to: {
+            route: { id: '/storybook' },
+            params: {},
+            url: new URL('http://localhost/storybook'),
+          },
+        },
+        updated: {
+          current: true,
+        },
+      },
+    },
+  }}
+/>
